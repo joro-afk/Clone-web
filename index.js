5159e269c2bdd730fe1d6431ddc73d0f8fc97a4d
Joa <91098938+joro-afk@users.noreply.github.com>
2023-07-28 02:01:52 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 window.onscroll = function () {
   myFunction();
 };
-var navbar = document.getElementById("principal");
+let navbar = document.getElementById("principal");
 
-var sticky = navbar.offsetTop;
+let sticky = navbar.offsetTop;
 
 function myFunction() {
   if (window.pageYOffset >= sticky) {
@@ -18,55 +18,6 @@ function addClass() {
   element.className = element.className === "active" ? "inactive" : "active";
 }
 
-let slideIndex = 0;
-
-let slideIndex2 = 1;
-showSlides();
-
-function showSlides() {
-  let i;
-  let slides = document.getElementsByClassName("img-container-1");
-  let dots = document.getElementsByClassName("slider-1-container-mini-img");
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
-  }
-  slideIndex++;
-  if (slideIndex > slides.length) {
-    slideIndex = 1;
-  }
-  for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" active", "");
-  }
-  slides[slideIndex - 1].style.display = "block";
-  dots[slideIndex - 1].style.className += " active";
-
-  setTimeout(showSlides, 50000);
-}
-
-function currentSlide(n) {
-  showSlides2((slideIndex = n));
-}
-
-function showSlides2(n) {
-  let i;
-  let slides = document.getElementsByClassName("img-container-1");
-  let dots = document.getElementsByClassName("slider-1-container-mini-img");
-  if (n > slides.length) {
-    slideIndex = 1;
-  }
-  if (n < 1) {
-    slideIndex = slides.length;
-  }
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
-  }
-  for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" active", "");
-  }
-  slides[slideIndex - 1].style.display = "block";
-  dots[slideIndex - 1].className += " active";
-}
-
 let slideIndex3 = 1;
 
 function currentSlide3(n) {
@@ -77,7 +28,7 @@ function showSlides3(n) {
   let i;
   let slides = document.getElementsByClassName("img-container-2");
   let dots = document.getElementsByClassName(
-    "slider-2-container-mini-content "
+    "slider-2-container-mini-content"
   );
   if (n > slides.length) {
     slideIndex3 = 1;
@@ -94,3 +45,4 @@ function showSlides3(n) {
   slides[slideIndex3 - 1].style.display = "inline";
   dots[slideIndex3 - 1].className += " active";
 }
+
